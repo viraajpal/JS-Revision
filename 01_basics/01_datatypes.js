f55630577111478b877b6b48c7509cb65b5fc7c1
Viraaj Pal <123467535+viraajpal@users.noreply.github.com>
2024-01-03 17:58:42 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+console.log("Viraaj")
+
+//number => 2 to the power 53
+//bigint
+// string => ""
+//boolean =>true/false
+//null=> standalone value
+//undefined =>
+//symbol => for uniueness 
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
