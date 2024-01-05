2a6c681ceaf6a83ade2698715873f2a9c155072e
Viraaj Pal <123467535+viraajpal@users.noreply.github.com>
2024-01-05 10:54:15 +0000

    datatypes-summary

---
+++ 01_basics/04_datatypes-summary.js
@@ -0,0 +1,32 @@
+//Primitive data types
+
+/* 
+
+7 types:
+
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+
+*/
+
+//Refernce (Non Primitive)
+
+//Array, Object, Function
+
+const heros = ["Abhay" , "Viraaj" , "Surya"]
+let myObj = {
+    name:"Viraaj",
+    age: 22,
+}
+console.log(myObj)
+console.log(heros)
+
+const myFunction = function(){
+    console.log("Hello Sir!")
+}
+myFunction()
