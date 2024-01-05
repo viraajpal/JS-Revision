d26ba31566a6f1e16d59a53a57c4f3f7bffc71f0
Viraaj Pal <123467535+viraajpal@users.noreply.github.com>
2024-01-05 17:33:02 +0000

    Strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,28 @@
+// const name = "Viraaj"
+// const repo = 67
+
+// // console.log(name + repo + " Abhay")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+const gameName = new String("Viraaj-VP")
+
+// console.log(gameName[4])
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2))
+
+// console.log(gameName.indexOf('j'))
+
+// console.log(gameName.substring(0, 3))
+
+// console.log(gameName.slice(-1, 4))
+
+// console.log(gameName.trim())
+
+// console.log(gameName.replace("V", "M"))
+
+// console.log(gameName.replace("V", "M").trim())
+
+console.log(gameName.split("-----"))
