c3c9b630fb7d5c3617701732f7a2754752b3553d
Viraaj Pal <123467535+viraajpal@users.noreply.github.com>
2024-01-11 16:12:53 +0530

    Create 07_nums_and_Math.js

---
+++ 01_basics/07_nums_and_Math.js
@@ -0,0 +1,55 @@
+const score = 400
+// console.log(score)
+
+// const balance = new Number(1000)
+// console.log(balance)
+
+// console.log(typeof balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(1))
+
+// const secondNumber = 354.6666
+// console.log(secondNumber.toPrecision(3))
+
+// const hundreds = 10000000000000
+// console.log(hundreds.toLocaleString())
+
+
+// const hundreds1 = 10000000000000
+// console.log(hundreds1.toLocaleString("en-IN"))
+
+/// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math); 
+// console.log(typeof Math)
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.abs(4))
+
+
+// console.log(Math.round(456.98778))
+
+// console.log(Math.floor(456.98778))
+
+// console.log(Math.ceil(456.98778))
+
+// console.log(Math.min(4,5, 8, 9, 77))
+// console.log(Math.max(4,5, 8, 9, 77))
+
+//++++++++++++++++++++++++++++++ Most Important +++++++++++++++++++++++++++++
+
+
+console.log(Math.random())
+
+console.log(Math.random()*10)
+
+console.log((Math.random()*10) + 1)
+
+const min = 10
+
+const max = 20
+ 
+console.log((Math.floor(Math.random() * (max - min + 1) + min)))
